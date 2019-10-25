abba916f36dea3a9784e12344badf9662a7b70e9
hanstheman <hey2k_bad@hushmail.com>
2019-10-25 21:06:16 +0200

    Changing to VS code

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const //ask player controller : what tank are you controlling ?
+{
+	return Cast<ATank>(GetPawn()); //type to type ATank the pawn that playercontroller is controlling..
+}
+
+
