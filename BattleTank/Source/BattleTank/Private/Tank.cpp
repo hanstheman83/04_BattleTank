a32446306e1a5f0078687d766988e5ed4361ea7c
hanstheman <hey2k_bad@hushmail.com>
2019-12-21 20:56:21 +0100

    Video 205

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -4,6 +4,8 @@
 #include "Tank.h"
 #include "TankAimingComponent.h"
 #include "Engine/World.h"
+#include "TankBarrel.h"
+#include "Projectile.h"
 
 
 
@@ -35,6 +37,7 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
 {
 	TankAimingComponent->SetBarrelReference(BarrelToSet);
+	Barrel = BarrelToSet;
 }
 
 void ATank::SetTurretReference(UTankTurret* TurretToSet)
@@ -49,7 +52,17 @@ void ATank::AimAt(FVector HitLocation)
 
 void ATank::Fire()
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Tank fires"), Time);
+	
+
+	if (!Barrel) { return; }
+
+	// spawn a projectile at the socket location on the barrel
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
+		ProjectileBlueprint,
+		Barrel->GetSocketLocation(FName("Projectile")),
+		Barrel->GetSocketRotation(FName("Projectile"))
+		);
+
+	Projectile->LaunchProjectile(LaunchSpeed);
 
 }
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -9,6 +9,7 @@
 
 class UTankBarrel; // forward declaration
 class UTankAimingComponent; // forward declaration
+class AProjectile;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -45,4 +46,9 @@ private:
 	UPROPERTY(EditAnywhere, Category = Firing)
 	float LaunchSpeed = 4000; // 1000 m/s TODO find right speed!
 	
+	UPROPERTY(EditAnywhere, Category = Setup)
+	TSubclassOf<AProjectile> ProjectileBlueprint;
+
+	// Local barrel reference for spawning projectile
+	UTankBarrel* Barrel = nullptr;
 };
